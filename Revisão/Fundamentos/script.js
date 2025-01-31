733e125802122099ebee43529d4d1547489e7840
GiulioArantes <giulio.arantes@icloud.com>
2025-01-31 17:18:50 -0300

    Revisão de conceitos fundamentais(semântica, flexbox, DOM e localStorage)

---
+++ Revisão/Fundamentos/script.js
@@ -0,0 +1,30 @@
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+    const name = document.getElementById("name").vale;
+    const email = document.getElementById("email").vale;
+    const message = document.getElementById("message").vale;
+
+    if (!name || !email || !message) {
+        alert(`Por favor, preencha todos os campos.`);
+        event.preventDefault();
+    }
+});
+
+document.getElementById("toggle-button").addEventListener("click", function () {
+    const text = document.getElementById("hidden-text");
+    if (text.style.display === "none") {
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+});
+
+document.getElementById("save-button").addEventListener("click", function () {
+    const input = document.getElementById("user-input").value;
+    localStorage.setItem("savedText", input);
+    document.getElementById("saved-text").textContent = `Texto salvo: ${input}`;
+})
+
+const savedText = localStorage.getItem("savedText");
+if (savedText) {
+    document.getElementById("saved-text").textContent = `Texto salvo: ${savedText}`;
+}
